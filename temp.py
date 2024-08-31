625c33d7298fbb849a96b4e0d5974f1adb119c84
enidev911 <enidev911@gmail.com>
2024-08-31 18:11:20 -0400

    change: add function filter by comuna

--- temp.py
+++ temp.py
@@ -15,4 +15,25 @@ def get_inmuebles_by_name_and_description(name, descr):
             xfile.write(str(inmueble))
             xfile.write("\n")
 
-get_inmuebles_by_name_and_description("Providencia", "Cocina")
+# get_inmuebles_by_name_and_description("Providencia", "Cocina")
+
+def get_inmuebles_by_comuna(comuna):
+    query = "SELECT inm.id, inm.nombre_inmueble, inm.descripcion"
+    query += " FROM arrienda_ya_inmueble AS inm"
+    query += " INNER JOIN arrienda_ya_region AS reg"
+    query += " ON inm.id_region_id = reg.id"
+    query += " INNER JOIN arrienda_ya_comuna AS com"
+    query += " ON inm.id_comuna_id = com.id"
+    query += f" WHERE com.comuna LIKE '%%{str(comuna)}%%'"
+
+    results = Inmueble.objects.raw(query)
+        
+    with open("datos.txt", "w") as xfile:
+        for result in results:
+          xfile.write(result.nombre_inmueble+", "+result.descripcion)
+          xfile.write("\n")
+          
+get_inmuebles_by_comuna("Bernardo")
+
+
+    
