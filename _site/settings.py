2bdb9ce6d4e750f4dc8b2f18c494d10ad3136b20
enidev911 <enidev911@gmail.com>
2024-08-31 05:11:00 -0400

    change: update config db

--- _site/settings.py
+++ _site/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = "_site.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "arriendaya", # 👈 base de datos postgres
+        "USER": "postgres", # 👈 usuario para postgres
+        "PASSWORD": "postgres", # 👈 password para postgres
+        "HOST": "localhost",
+        "PORT": 5432
     }
 }
 
