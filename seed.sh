62d7b09394f37c91a18c5c317c18cf8f39bf7741
enidev911 <enidev911@gmail.com>
2024-08-31 06:22:48 -0400

    add: seed.sh

---
+++ seed.sh
@@ -0,0 +1,5 @@
+python manage.py loaddata comunas
+python manage.py loaddata regiones
+python manage.py loaddata tipos_inmuebles
+python manage.py loaddata tipos_usuarios
+python manage.py loaddata inmuebles
